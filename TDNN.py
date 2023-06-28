9cafcf568b54fdf627f049975e31d1bd3c022ced
Yip-Jia-Qi <71979039+Yip-Jia-Qi@users.noreply.github.com>
2023-06-28 12:55:26 +0800

    Create TDNN.py

---
+++ TDNN.py
@@ -0,0 +1,77 @@
+"""TDNN Block from SpeechBrain ECAPA-TDNN model
+Authors
+ * Hwidong Na 2020
+"""
+
+# import os
+import torch  # noqa: F401
+import torch.nn as nn
+import torch.nn.functional as F
+from speechbrain.dataio.dataio import length_to_mask
+from speechbrain.nnet.CNN import Conv1d as _Conv1d
+from speechbrain.nnet.normalization import BatchNorm1d as _BatchNorm1d
+from speechbrain.nnet.linear import Linear
+
+# Skip transpose as much as possible for efficiency
+class Conv1d(_Conv1d):
+    """1D convolution. Skip transpose is used to improve efficiency."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(skip_transpose=True, *args, **kwargs)
+
+
+class BatchNorm1d(_BatchNorm1d):
+    """1D batch normalization. Skip transpose is used to improve efficiency."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(skip_transpose=True, *args, **kwargs)
+
+
+class TDNNBlock(nn.Module):
+    """An implementation of TDNN.
+    Arguments
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        The number of output channels.
+    kernel_size : int
+        The kernel size of the TDNN blocks.
+    dilation : int
+        The dilation of the TDNN block.
+    activation : torch class
+        A class for constructing the activation layers.
+    groups: int
+        The groups size of the TDNN blocks.
+    Example
+    -------
+    >>> inp_tensor = torch.rand([8, 120, 64]).transpose(1, 2)
+    >>> layer = TDNNBlock(64, 64, kernel_size=3, dilation=1)
+    >>> out_tensor = layer(inp_tensor).transpose(1, 2)
+    >>> out_tensor.shape
+    torch.Size([8, 120, 64])
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        dilation,
+        activation=nn.ReLU,
+        groups=1,
+    ):
+        super(TDNNBlock, self).__init__()
+        self.conv = Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            dilation=dilation,
+            groups=groups,
+        )
+        self.activation = activation()
+        self.norm = BatchNorm1d(input_size=out_channels)
+
+    def forward(self, x):
+        """ Processes the input tensor x and returns an output tensor."""
+        return self.norm(self.activation(self.conv(x)))
